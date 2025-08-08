db30e2813f2717f508b0ed88e996222e5c03d61d
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-08-08 10:45:54 +0100

    Fix incorrect installation detection

--- scripts/git-hook.sh
+++ scripts/git-hook.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! 'helm plugin list | grep -q "schema"' > /dev/null 2>&1; then
+if 'helm plugin list | grep -q "schema"' > /dev/null 2>&1; then
     echo "Please install helm-values-schema-json plugin! https://github.com/losisin/helm-values-schema-json#install"
 fi
 
