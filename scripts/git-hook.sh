bec568993effe90c6c07e0406e913fb0e81a3062
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-08-08 11:04:49 +0100

    Change return to exit in uninstalled case

--- scripts/git-hook.sh
+++ scripts/git-hook.sh
@@ -4,7 +4,7 @@ set -e
 
 if 'helm plugin list | grep -q "schema"' > /dev/null 2>&1; then
     echo "Please install helm-values-schema-json plugin! https://github.com/losisin/helm-values-schema-json#install"
-    return 1
+    exit 1
 fi
 
 helm schema "${@}"
